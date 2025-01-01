83ce13af7fbcf869e210277ef3f6a06b13ed5321
WarperSan <leumas.ecole@gmail.com>
2025-01-01 17:41:05 -0500

    forgot the files -.-

---
+++ Compatibility/CustomItemBehaviourLibraryCompatibility.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using CustomItemBehaviourLibrary.AbstractItems;
+using ShipInventory.Helpers;
+using ShipInventory.Objects;
+
+namespace ShipInventory.Compatibility;
+
+public static class CustomItemBehaviourLibraryCompatibility
+{
+    public const string CUSTOM_ITEM_BEHAVIOUR_LIBRARY = "com.github.WhiteSpike.CustomItemBehaviourLibrary";
+    private static bool? _enabled;
+
+    public static bool Enabled
+    {
+        get
+        {
+            _enabled ??= BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey(CUSTOM_ITEM_BEHAVIOUR_LIBRARY);
+            return _enabled.Value;
+        }
+    }
+
+    public static ItemData[]? ConvertContainer(GrabbableObject item) => Enabled ? HandleContainer(item) : null;
+
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    public static ItemData[]? HandleContainer(GrabbableObject item)
+    {
+        if (item is not ContainerBehaviour container)
+            return null;
+
+        container.enabled = false;
+
+        var items = new List<ItemData>();
+        var subItems = container.GetComponentsInChildren<GrabbableObject>();
+
+        for (int i = subItems.Length - 1; i >= 0; i--)
+        {
+            var subItem = subItems[i];
+            
+            if (subItem == null)
+                continue;
+            
+            if (subItem == container)
+                continue;
+            
+            items.AddRange(ConvertItemHelper.ConvertItem(subItem));
+        }
+        
+        items.Add(new ItemData(container));
+        
+        return items.ToArray();
+    }
+}
---
+++ Helpers/ConvertItemHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using ShipInventory.Compatibility;
+using ShipInventory.Objects;
+
+namespace ShipInventory.Helpers;
+
+public static class ConvertItemHelper
+{
+    static ConvertItemHelper()
+    {
+        AddConversion(CustomItemBehaviourLibraryCompatibility.ConvertContainer);
+        AddConversion(BeltBagConversion);
+        AddConversion(NormalConversion, false);
+    }
+    
+    #region Conversions
+
+    private static readonly List<Func<GrabbableObject, ItemData[]?>> conversions = [];
+    
+    public static ItemData[] ConvertItem(GrabbableObject item)
+    {
+        ItemData[]? items = null;
+        
+        foreach (var conversion in conversions)
+        {
+            if (conversion == null)
+                continue;
+
+            items = conversion.Invoke(item);
+            
+            if (items != null)
+                break;
+        }
+
+        return items ?? [];
+    }
+    
+    /// <summary>
+    /// Adds a conversion that defines how the given object is converted to its data form
+    /// </summary>
+    /// <param name="conversion">Returns the list of data for the given object or null if this conversion doesn't handle this object</param>
+    /// <param name="appendToStart"></param>
+    public static void AddConversion(Func<GrabbableObject, ItemData[]?> conversion, bool appendToStart = true)
+    {
+        int index = appendToStart ? 0 : conversions.Count;
+        conversions.Insert(index, conversion);
+    }
+
+    private static ItemData[]? BeltBagConversion(GrabbableObject item)
+    {
+        if (item is not BeltBagItem beltBagItem)
+            return null;
+        
+        var items = new List<ItemData>();
+
+        for (int i = beltBagItem.objectsInBag.Count - 1; i >= 0; i--)
+        {
+            var _item = beltBagItem.objectsInBag[i];
+
+            if (_item == null)
+                continue;
+            
+            items.AddRange(ConvertItem(_item));
+        }
+
+        items.Add(new ItemData(beltBagItem));
+        
+        return items.ToArray();
+    }
+    private static ItemData[] NormalConversion(GrabbableObject item) => [new ItemData(item)];
+
+    #endregion
+}
