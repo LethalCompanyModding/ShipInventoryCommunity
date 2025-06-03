75a6235835f7825f86182cc0912286085b66d8cb
WarperSan <leumas.ecole@gmail.com>
2025-06-03 19:15:08 -0400

    Write test

--- src/ShipInventory/Constants.cs
+++ src/ShipInventory/Constants.cs
@@ -8,4 +8,5 @@ abstract class Constants
     // --- ASSETS ---
     public const string CHUTE_PREFAB = "ChutePrefab";
     public const string INVENTORY_BUY_TERMINAL_NODE = "InventoryBuy";
+    public const string t = "T";
 }
