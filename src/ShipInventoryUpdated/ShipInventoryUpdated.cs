35bd69a9330e60f475ab2b792ca8bd7ff2b50ffd
WarperSan <leumas.ecole@gmail.com>
2025-06-05 09:27:37 -0400

    Clean

---
+++ src/ShipInventoryUpdated/ShipInventoryUpdated.cs
@@ -0,0 +1,11 @@
+using BepInEx;
+
+namespace ShipInventoryUpdated;
+
+[BepInPlugin(LCMPluginInfo.PLUGIN_GUID, LCMPluginInfo.PLUGIN_NAME, LCMPluginInfo.PLUGIN_VERSION)]
+public class ShipInventoryUpdated : BaseUnityPlugin
+{
+    private void Awake()
+    {
+    }
+}
