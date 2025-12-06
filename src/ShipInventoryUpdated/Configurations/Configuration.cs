39c12e70040e18818f7ccf5d8c510c3124d2e490
WarperSan <leumas.ecole@gmail.com>
2025-12-06 15:56:23 -0500

    Added blank `MaxItemCount`

--- src/ShipInventoryUpdated/Configurations/Configuration.cs
+++ src/ShipInventoryUpdated/Configurations/Configuration.cs
@@ -10,12 +10,14 @@ internal class Configuration
 	public readonly ChuteConfig Chute;
 	public readonly UnlockConfig Unlock;
 	public readonly TerminalConfig Terminal;
+	public readonly InventoryConfig Inventory;
 
 	private Configuration(ConfigFile cfg)
 	{
 		Chute = new ChuteConfig(cfg);
 		Unlock = new UnlockConfig(cfg);
 		Terminal = new TerminalConfig(cfg);
+		Inventory = new InventoryConfig(cfg);
 	}
 
 	/// <summary>
---
+++ src/ShipInventoryUpdated/Configurations/InventoryConfig.cs
@@ -0,0 +1,23 @@
+using BepInEx.Configuration;
+using ShipInventoryUpdated.Helpers;
+
+namespace ShipInventoryUpdated.Configurations;
+
+/// <summary>
+/// Class that holds the configurations related to the inventory itself
+/// </summary>
+internal class InventoryConfig
+{
+	private const string SECTION = "Inventory";
+
+	public readonly ConfigEntry<int> MaxItemCount;
+
+	public InventoryConfig(ConfigFile cfg)
+	{
+		MaxItemCount = cfg.Bind(
+			new ConfigDefinition(SECTION, "MaxItemCount"),
+			5_000,
+			new ConfigDescription(Localization.Get("configuration.inventory.maxItemCount.description"))
+		);
+	}
+}
