1b84d235ef2839d5f074639d065408b38b8c6c10
warpersan <leumas.ecole@gmail.com>
2026-01-24 13:56:55 -0500

    Made `MaxItemCount`, `ClearOnWipe` and `ChuteDelay` synced

--- src/ShipInventoryUpdated/Configurations/InventoryConfig.cs
+++ src/ShipInventoryUpdated/Configurations/InventoryConfig.cs
@@ -1,4 +1,6 @@
 using BepInEx.Configuration;
+using CSync.Extensions;
+using CSync.Lib;
 using ShipInventoryUpdated.Helpers;
 
 namespace ShipInventoryUpdated.Configurations;
@@ -11,28 +13,30 @@ internal class InventoryConfig : SyncedConfig2<InventoryConfig>
 	private const string GUID_ = MyPluginInfo.PLUGIN_GUID + "." + nameof(InventoryConfig);
 	private const string SECTION = "Inventory";
 
-	public readonly ConfigEntry<int> MaxItemCount;
-	public readonly ConfigEntry<bool> ClearOnWipe;
-	public readonly ConfigEntry<float> RetrieveSpeed;
+	[SyncedEntryField] public readonly SyncedEntry<int> MaxItemCount;
+	[SyncedEntryField] public readonly SyncedEntry<bool> ClearOnWipe;
+	[SyncedEntryField] public readonly SyncedEntry<float> RetrieveSpeed;
 
 	public InventoryConfig(ConfigFile cfg) : base(GUID_)
 	{
-		MaxItemCount = cfg.Bind(
+		MaxItemCount = cfg.BindSyncedEntry(
 			new ConfigDefinition(SECTION, "MaxItemCount"),
 			5_000,
 			new ConfigDescription(Localization.Get("configuration.inventory.maxItemCount.description"))
 		);
 		
-		ClearOnWipe = cfg.Bind(
+		ClearOnWipe = cfg.BindSyncedEntry(
 			new ConfigDefinition(SECTION, "ClearOnWipe"),
 			true,
 			new ConfigDescription(Localization.Get("configuration.inventory.clearOnWipe.description"))
 		);
 
-		RetrieveSpeed = cfg.Bind(
+		RetrieveSpeed = cfg.BindSyncedEntry(
 			new ConfigDefinition(SECTION, "ChuteDelay"),
 			0.5f,
 			new ConfigDescription(Localization.Get("configuration.inventory.retrieveSpeed.description"))
 		);
+		
+		ConfigManager.Register(this); 
 	}
 }
